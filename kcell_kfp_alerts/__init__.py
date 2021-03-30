dcc31fb02a0941813b370f31b3e7a913d4628d11
Rauan Akylzhanov <rauan.akylzhanov@kcell.kz>
2021-03-30 09:17:16 +0000

    clean call to kfp.dsl.ContainerOp

--- kcell_kfp_alerts/__init__.py
+++ kcell_kfp_alerts/__init__.py
@@ -1,26 +1,24 @@
 import kfp
+from kubernetes.client.models import V1EnvVar, V1ResourceRequirements, V1Volume, V1HostPathVolumeSource, V1SecretVolumeSource
+from kcell_kfp_runners import HADOOP_VOLUMES
+ALERT_IMAGE="artifactory.kraken.kcell.kz:6555/datalake-email-alert:latest"
 
-def send_email(notebook, 
-               limits={"cpu": "1", "memory": "1Gi"}, 
-               outputs={}, args=[], 
-               requests=None, image="artifactory.kraken.kcell.kz:6555/datalake-email-alert:latest"):
-    if not requests:
-        requests = limits
+@kfp.dsl.component
+def send_email(name, title,  subject, body, args=[]):
     return kfp.dsl.ContainerOp(
-        name=notebook.split("/")[0],
-        image=image,
+        name=name,
+        image=ALERT_IMAGE,
         pvolumes=HADOOP_VOLUMES,
         arguments=args,
         container_kwargs={
-            "resources": k8s.V1ResourceRequirements(limits=limits, requests=requests),
+            "resources": k8s.V1ResourceRequirements(limits={"cpu": "1", "memory": "1Gi"}),
             "env": [
                 k8s.V1EnvVar("NOTEBOOK", "/opt/" + notebook),
             ],
         },
         file_outputs={
             **outputs,
-            "mlpipeline-ui-metadata": "/mlpipeline-ui-metadata.json",
-            "executed-notebook": "/result.ipynb",
-        },
+            "mlpipeline-ui-metadata": "/mlpipeline-ui-metadata.json"
+        }
     )
 
